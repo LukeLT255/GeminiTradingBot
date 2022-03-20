d976f454390e1fb63eda7bdc0fbc4491ddefcd59
LukeT <lukelt25@gmail.com>
2022-03-20 16:08:57 -0400

    Added setup.py and MIT license

---
+++ gemini/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Personal Use',
+    'Operating System :: Apple :: MacOS :: MacOS Mojave',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='gemini',
+    version='0.0.1',
+    description="Modules for working with Gemini's API ina pythonic way",
+    long_description="",
+    url='',
+    author='Luke Tyson',
+    author_email='LukeLT25@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='gemini',
+    packages=find_packages(),
+    install_requires=[open('requirements.txt').read()]
+
+)
